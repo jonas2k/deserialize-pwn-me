e59bfbba1cc49956ce93b25546becac393f3e4f1
jonas2k <jonas2k>
2017-01-27 14:20:41 +0000

    Further enhancements

--- src/main/java/de/hskl/servlet/IndexServlet.java
+++ src/main/java/de/hskl/servlet/IndexServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import org.apache.commons.codec.binary.Base64;
 
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
@@ -19,41 +19,38 @@ public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = -789670430012592030L;
 
 	public IndexServlet() {
-        super();
-    }
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+
 		String input = request.getParameter("input");
-				
-		if (input == null) {
-			request.setAttribute("error", "Error!");
+
+		if (input == null || !Base64.isBase64(input.getBytes())) {
+			request.setAttribute("error", "Error: Input empty or not base64 encoded!");
 		} else {
-			
-			byte[] data = Base64.decode(input);
-			
-			try {
-				ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+
+			byte[] data = Base64.decodeBase64(input);
+
+			try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
 				
-				try {
-					Object object = ois.readObject();
-					request.setAttribute("success", "Success!");
-					request.setAttribute("object", "Object info: "+object.toString());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				Object object = ois.readObject();
+				request.setAttribute("success", "Success!");
+				request.setAttribute("object", "Object info: " + object.toString());
 				ois.close();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
-	    dispatcher.forward(request, response);
+		dispatcher.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
